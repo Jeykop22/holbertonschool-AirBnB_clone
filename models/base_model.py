9c4f5763c984271a8327521ad42ec18906435bfc
Jeykop22 <nolanbinet2@gmail.com>
2024-03-05 09:50:33 +0000

    change base

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
-import models
+"""Write a class BaseModel that defines all common attributes/methods
+for other classes"""
+
 import uuid
 from datetime import datetime
-"""Write a class BaseModel that defines all common attributes/methods
-for other classes:"""
-
 
 class BaseModel:
-    """BseModel with the principal function to inherit"""
+    """Write a class BaseModel that defines all common attributes/methods
+    for other classes"""
+
     def __init__(self):
+        """Initializes BaseModel instance"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        models.storage.new(self)
-
-    def save(self):
-        """saves a new version of class and updates the time of update"""
-        self.updated_at = datetime.now()
-        models.storage.save()
 
     def __str__(self):
-        """ Returns a readable string representation
-        of an instance"""
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+        """Returns a readable string representation of an instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        """To_dict function to have a dictionary version of data"""
+        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
         instance_dict = self.__dict__.copy()
-        instance_dict.update({"__class__": self.__class__.__name__})
-        instance_dict.update({"created_at": self.created_at.isoformat()})
-        instance_dict.update({"updated_at": self.updated_at.isoformat()})
+        instance_dict['__class__'] = self.__class__.__name__
+        instance_dict['created_at'] = self.created_at.isoformat()
+        instance_dict['updated_at'] = self.updated_at.isoformat()
         return instance_dict
+    
