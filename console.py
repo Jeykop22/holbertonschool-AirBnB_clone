8db597140ada870f6ac31e8efd28211a0b735f97
Jeykop22 <nolanbinet2@gmail.com>
2024-03-07 10:05:09 +0000

    modify console.py

--- console.py
+++ console.py
@@ -5,16 +5,9 @@ This module represents the console
 
 import cmd
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
 import json
 
-classes = {"BaseModel": BaseModel, "User": User, "State": State, "City": City,
-           "Amenity": Amenity, "Place": Place, "Review": Review}
+classes = {"BaseModel": BaseModel}
 
 
 class HBNBCommand(cmd.Cmd):
